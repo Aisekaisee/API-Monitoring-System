d88f60e334590c70fdaff1e6b0704c1ec7307635
Aisekaisee <kohlivipul48@gmail.com>
2026-04-14 01:52:29 +0530

    universal error handler

--- server/src/shared/utils/AppError.js
+++ server/src/shared/utils/AppError.js
@@ -1,3 +1,8 @@
+/**
+ * App Error - Custome error class for handling application-specfic errors with additional context.
+ * This class can be extended in the future to include additional properties or methods as needed.
+ */
+ 
 class AppError extends Error {
     constructor(message, statusCode = 500 , errors = null) {
         super(message);
